0e9040cdf87c329d5668d7ad16d5c19a215aa067
YuhaoZhang-tirsky <tirsky@outlook.com>
2020-09-02 18:25:26 +1000

    add loading animation

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,38 @@ import Layout from '../components/Layout'
 import '../style/style.css'
 import { Provider } from 'react-redux'
 import withReduxComp from '../lib/with-redux'
+import PageLoading from '../components/PageLoading'
+import Router from 'next/router'
+import Link from 'next/link'
 
 class MyApp extends App {
   state = {  
-    context: 'value'
+    context: 'value',
+    loading: false
+  }
+
+  startLoading = () => {
+    this.setState({
+      loading: true
+    })
+  }
+
+  stopLoading = () => {
+    this.setState({
+      loading: false
+    })
+  }
+
+  componentDidMount() {
+    Router.events.on('routeChangeStart', this.startLoading)
+    Router.events.on('routeChangeComplete', this.stopLoading)
+    Router.events.on('routeChangeError', this.stopLoading)
+  }
+
+  componentWillUnmount() {
+    Router.events.off('routeChangeStart', this.startLoading)
+    Router.events.off('routeChangeComplete', this.stopLoading)
+    Router.events.off('routeChangeError', this.stopLoading)
   }
 
   static async getInitialProps (ctx) {
@@ -24,7 +52,10 @@ class MyApp extends App {
     return(
       <Container>
         <Provider store={reduxStore}>
+          {this.state.loading ? <PageLoading /> : null}
           <Layout>
+            <Link href="/"><a>Index</a></Link>
+            <Link href="/detail"><a>Detail</a></Link>
             <Component {...pageProps} />
           </Layout>
         </Provider>
--- pages/detail.js
+++ pages/detail.js
@@ -1,1 +1,13 @@
-export default () => <span>detail</span>
+const Detail = () => {
+  return <span>Detail</span>
+}
+
+Detail.getInitialProps = async () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve({})
+    }, 1000);
+  })
+}
+
+export default Detail
